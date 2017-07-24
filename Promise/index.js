7e7a89dd7759e2dab71c4ffa4949191d83c1c264
MechanicianW <wangyilinchn@foxmail.com>
2017-07-24 21:33:22 +0800

    Add A+ test☺🐒

--- Promise/index.js
+++ Promise/index.js
@@ -16,15 +16,19 @@ function MPromise () {
                     if (this.state === 1) {
                         if (onFulfilled && typeof(onFulfilled) === 'function') {
                             // must be called after promise is fulfilled, with promise’s value as its first argument.
+                            this.reslove(onFulfilled(this.value));
                         } else {
                             // must be fulfilled with the same value as promise1.
+                            this.reslove(this.value);
                         }
                     }
                     if (this.state === 2) {
                         if (onRejected && typeof(onRejected) === 'function') {
                             // it must be called after promise is rejected, with promise’s reason as its first argument.
+                            this.reject(onRejected(this.reason));
                         } else {
                             // must be rejected with the same reason as promise1.
+                            this.reject(this.reason);
                         }
                     }
                 })
@@ -32,13 +36,42 @@ function MPromise () {
             if (this.state) {
                 thenWork();
             } else {
-                this.callbackQueue.push(thenWork)
+                this.callbackQueue.push(thenWork);
             }
         })
     }
 
     this.resolve = function (value) {
+        // 状态流转到 reslove
         if (this.state) return;
-        if (value === this) return this.rejected(new TypeError('refer to the same object'))
+        if (value === this) return this.reject(new TypeError('refer to the same object'));
+        if (typeof value === 'object' || typeof value === 'function') {
+            try {
+                let then = value.then;
+                if (typeof then === 'function') {
+                    then.call(value);
+                }
+            } catch (e) {
+                this.reject(e);
+            }
+        }
+        this.state = stateMap.get('reslove');
+        this.value = value;
+        this.callbackQueue.forEach((cb) => {
+            cb(this);
+        })
+    }
+
+    this.reject = function (reason) {
+        // 状态流转到reject
+        if (this.state) return;
+        if (reason === this) return this.reject(new TypeError('refer to the same object'));
+        this.state = stateMap.get('rejected');
+        this.reason = reason;
+        this.callbackQueue.forEach((cb) => {
+            cb(this);
+        })
     }
 }
+
+module.exports = MPromise;
